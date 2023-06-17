fdf8554b011d2efacec19e7cb1e1c74b8dc8c6df
timothyngugi26 <timothyngugi26@gmail.com>
2023-06-17 02:36:19 -0700

    Hello World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main Entry-points
+ *
+ * Return; Always 0 (success)
+ */
+int  main (void)
+{printf("Programming is like building a multilingual puzzle\n");
+   return (0)	
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/** Main-Entry point
+ *
+ * Return 0;(success)
+ **/
+int main(void)
+{
+printf("Size of the char:%d byte(s)\n", sizeof (char));
+printf("size of an int:%d byte(s)\n",sizeof (int));
+printf("size of the long:%d byte(s)\n",sizeof (long int));
+printf("size of the long long int.:%d byte(s)\n",sizeof(long long int));
+printf("size of float:%d byte(s)\n", sizeof (float));
+return (0);
+}
