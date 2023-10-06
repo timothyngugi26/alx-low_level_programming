26269a65071a11fd2a19774d784b7823c58a765b
Your Name <you@example.com>
2023-10-05 21:10:59 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,19 @@
+#!/bin/bash
+gcc $CFILE -o cisfun
+
+4-puts.c
+
 #include<stdio.h>
 /**
- * main Entry-points
+ * main - Entry point
  *
- * Return; Always 0 (success)
+ * Return: Always 0 (Success)
  */
-int  main (void)
-{printf("Programming is like building a multilingual puzzle\n");
-   return (0)	
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
+
+
