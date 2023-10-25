c2a706856df114eb55c8c9b091d0bc90acfdbef6
Your Name <you@example.com>
2023-10-25 13:12:40 -0700

    C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,12 @@
-#include <stdio.h>
-
-void _puts_recursion(char *s) {
-    if (*s == '\0') {
-        putchar('\n'); // Print a newline when the end of the string is reached
-        return;
-    }
-
-    putchar(*s); // Print the current character
-    _puts_recursion(s + 1); // Recursively call the function with the next character
-}
-
-int main() {
-    char str[] = "Hello, World!";
-    _puts_recursion(str);
-    return 0;
+#include <unistd.h>
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
 }
