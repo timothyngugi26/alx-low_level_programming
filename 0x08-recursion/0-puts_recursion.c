e9bf19b67a6a4af96d6fd1adfe736780b8174712
Your Name <you@example.com>
2023-10-26 09:12:14 -0700

    0x08. C - Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,22 @@
-#include <unistd.h>
+#include "main.h"
+
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c)
-{
-return (write(1, &c, 1));
+ * _puts_recursion - recursive function to print a string to the screen
+ *
+ * @s: pointer to the string
+ *
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{       
+    if (*s != '\0')
+    {       
+        _putchar(*s);
+        _puts_recursion(s + 1);
+    }
+    else
+    {
+        _putchar('\n');
+    }       
 }
