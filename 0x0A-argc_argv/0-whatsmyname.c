ad45b7ef6956062d90ec5923ed67198e55c5ba57
Your Name <you@example.com>
2023-10-28 00:38:41 -0700

    program  printing the  name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print its name followed by a new line
+ *
+ * @argc: number of command line arguments.
+ * @argv: array that contains the command line args.
+ *
+ * Return: 0 when successful
+ **/
+int main(int argc __attribute__((unused)), char *argv[])
+{
+   printf("%s\n", argv[0]);
+   return 0;
+}
+
